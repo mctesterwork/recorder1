5777c72728a876f50e6b5d2aceebb48f8c0e9f28
Miguel Correa <mctesterwork@gmail.com>
2023-03-26 11:49:17 -0500

    Little changes to design, added quality property (hardcoded for now)

--- recorder1/Form1.Designer.cs
+++ recorder1/Form1.Designer.cs
@@ -28,27 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Record = new System.Windows.Forms.Button();
             this.cbox_inputDevices = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.cbox_outputDevices = new System.Windows.Forms.ComboBox();
+            this.chbox_Input = new System.Windows.Forms.CheckBox();
+            this.chbox_Output = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Browse = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_Record
             // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(214, 105);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(171, 72);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "RECORD";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_Record.BackColor = System.Drawing.Color.OrangeRed;
+            this.btn_Record.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Record.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Record.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_Record.Location = new System.Drawing.Point(214, 105);
+            this.btn_Record.Name = "btn_Record";
+            this.btn_Record.Size = new System.Drawing.Size(171, 72);
+            this.btn_Record.TabIndex = 0;
+            this.btn_Record.Text = "RECORD";
+            this.btn_Record.UseVisualStyleBackColor = false;
+            this.btn_Record.Click += new System.EventHandler(this.btn_Record_Click);
             // 
             // cbox_inputDevices
             // 
             this.cbox_inputDevices.FormattingEnabled = true;
-            this.cbox_inputDevices.Location = new System.Drawing.Point(12, 53);
+            this.cbox_inputDevices.Location = new System.Drawing.Point(12, 66);
             this.cbox_inputDevices.Name = "cbox_inputDevices";
             this.cbox_inputDevices.Size = new System.Drawing.Size(176, 23);
             this.cbox_inputDevices.TabIndex = 1;
@@ -57,32 +66,99 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.Size = new System.Drawing.Size(111, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "Recording Devices";
             // 
             // cbox_outputDevices
             // 
             this.cbox_outputDevices.FormattingEnabled = true;
-            this.cbox_outputDevices.Location = new System.Drawing.Point(12, 108);
+            this.cbox_outputDevices.Location = new System.Drawing.Point(12, 125);
             this.cbox_outputDevices.Name = "cbox_outputDevices";
             this.cbox_outputDevices.Size = new System.Drawing.Size(176, 23);
             this.cbox_outputDevices.TabIndex = 3;
             this.cbox_outputDevices.SelectedIndexChanged += new System.EventHandler(this.cbox_outputDevices_SelectedIndexChanged);
+            // 
+            // chbox_Input
+            // 
+            this.chbox_Input.AutoSize = true;
+            this.chbox_Input.Location = new System.Drawing.Point(12, 41);
+            this.chbox_Input.Name = "chbox_Input";
+            this.chbox_Input.Size = new System.Drawing.Size(86, 19);
+            this.chbox_Input.TabIndex = 5;
+            this.chbox_Input.Text = "Record Mic";
+            this.chbox_Input.UseVisualStyleBackColor = true;
+            // 
+            // chbox_Output
+            // 
+            this.chbox_Output.AutoSize = true;
+            this.chbox_Output.Location = new System.Drawing.Point(12, 100);
+            this.chbox_Output.Name = "chbox_Output";
+            this.chbox_Output.Size = new System.Drawing.Size(151, 19);
+            this.chbox_Output.TabIndex = 6;
+            this.chbox_Output.Text = "Record computer audio";
+            this.chbox_Output.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(214, 70);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(87, 19);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Show clicks";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(214, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Video output";
+            // 
+            // btn_Browse
+            // 
+            this.btn_Browse.Location = new System.Drawing.Point(310, 38);
+            this.btn_Browse.Name = "btn_Browse";
+            this.btn_Browse.Size = new System.Drawing.Size(75, 23);
+            this.btn_Browse.TabIndex = 9;
+            this.btn_Browse.Text = "browse";
+            this.btn_Browse.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "...";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(397, 189);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_Browse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.chbox_Output);
+            this.Controls.Add(this.chbox_Input);
             this.Controls.Add(this.cbox_outputDevices);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbox_inputDevices);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Record);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Tiny Screen Recorder";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -91,9 +167,15 @@
 
         #endregion
 
-        private Button button1;
+        private Button btn_Record;
         private ComboBox cbox_inputDevices;
         private Label label1;
         private ComboBox cbox_outputDevices;
+        private CheckBox chbox_Input;
+        private CheckBox chbox_Output;
+        private CheckBox checkBox1;
+        private Label label2;
+        private Button btn_Browse;
+        private Label label3;
     }
 }
--- recorder1/Form1.cs
+++ recorder1/Form1.cs
@@ -1,15 +1,25 @@
+using ScreenRecorderLib;
+
 namespace recorder1
 {
     public partial class Form1 : Form
     {
+        private bool isRecording = false;
+        private Recorder _rec = Recorder.CreateRecorder();
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btn_Record_Click(object sender, EventArgs e)
         {
-
+            if (isRecording == false)
+            {
+                startRecording();
+            }
+            else { 
+                stopRecording();
+            }
         }
 
         private void cbox_inputDevices_SelectedIndexChanged(object sender, EventArgs e)
@@ -50,6 +60,46 @@ namespace recorder1
         private void cbox_outputDevices_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        // Start recording function
+        private void startRecording()
+        {
+            // First we need to set the recorder options
+            this.btn_Record.Enabled = false;
+            var opts = new RecorderOptions
+            {
+                AudioOptions = new AudioOptions
+                {
+                    AudioInputDevice = Recorder.GetSystemAudioDevices(AudioDeviceSource.InputDevices).First().DeviceName,
+                    AudioOutputDevice = null,
+                    IsAudioEnabled = true,
+                    IsInputDeviceEnabled = true,
+                    IsOutputDeviceEnabled = false,
+                },
+                VideoEncoderOptions = new VideoEncoderOptions
+                {
+                    Quality = 100
+                }
+            };
+            _rec.SetOptions(opts);
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss");
+            string filePath = Path.Combine(Path.GetTempPath(), "ScreenRecorder", timestamp, timestamp + ".mp4");
+            _rec.Record(filePath);
+            isRecording = true;
+            this.btn_Record.Text = "STOP";
+            this.btn_Record.Enabled = true;
+
+        }
+
+        // Stop recording function. TODO: need to make this better using async and cancel token
+        private void stopRecording()
+        {
+            this.btn_Record.Enabled = false;
+            _rec.Stop();
+            this.btn_Record.Text = "RECORD";
+            this.btn_Record.Enabled = true;
+            isRecording = false;
         }
     }
 }
